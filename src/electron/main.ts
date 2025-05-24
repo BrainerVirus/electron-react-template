877b0f5cf822dfe9bf0e7c135e64f87966c44fa1
Cristhofer Pincetti <86972354+BrainerVirus@users.noreply.github.com>
2025-05-24 11:09:01 -0400

    fix: apply consistent formatting and fix import statements across multiple files

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,17 +1,17 @@
-import path from 'node:path'
-import { BrowserWindow, app } from 'electron'
-import { isDev } from './util.js'
+import { BrowserWindow, app } from 'electron';
+import path from 'node:path';
+import { isDev } from './util.js';
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({})
+	const mainWindow = new BrowserWindow({});
 
-  if (isDev()) {
-    // Load the file with the hash fragment pointing to home route
-    mainWindow.loadURL('http://localhost:5123')
-  } else {
-    // Load the file with the hash fragment pointing to home route
-    mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'), {
-      hash: '/',
-    })
-  }
-})
+	if (isDev()) {
+		// Load the file with the hash fragment pointing to home route
+		mainWindow.loadURL('http://localhost:5123');
+	} else {
+		// Load the file with the hash fragment pointing to home route
+		mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'), {
+			hash: '/',
+		});
+	}
+});
