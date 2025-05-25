a121a6e1df2911df1168c168d0939346b8d9e7b7
Cristhofer Pincetti <86972354+BrainerVirus@users.noreply.github.com>
2025-05-25 14:06:32 -0400

    feat: add custom header max length rule to commitlint configuration

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -5,5 +5,7 @@ module.exports = {
 		'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
 		// Add this rule to override the default max line length for the body
 		'body-max-line-length': [2, 'always', 500],
+		// Add this rule to override the default max line length for the header
+		'header-max-length': [2, 'always', 300],
 	},
 };
